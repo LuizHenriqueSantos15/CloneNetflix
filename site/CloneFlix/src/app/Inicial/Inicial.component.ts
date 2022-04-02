adaedbfe89b65fe088a8daf882c17fa72a38bc8b
Luiz Henrique Santos <96305078+LuizHenriqueSantos15@users.noreply.github.com>
2022-04-02 12:17:26 -0300

    Criando tela inicial, com responsividade

---
+++ site/CloneFlix/src/app/Inicial/Inicial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Inicial',
+  templateUrl: './Inicial.component.html',
+  styleUrls: ['./Inicial.component.css']
+})
+export class InicialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
